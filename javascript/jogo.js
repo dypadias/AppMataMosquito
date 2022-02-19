6139f7e7cc8e1687dcdbe76074673d32c9895e7e
Diego Matos <dypadias@gmail.com>
2022-02-18 22:17:43 -0300

    App mata mosquito

---
+++ javascript/jogo.js
@@ -0,0 +1,116 @@
+var altura = 0
+var largura = 0
+var vidas = 1
+var pontos = 0
+var recorde = 0
+var tempo = 20
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal') {
+    criaMosquitoTempo = 1500
+} else if (nivel === 'dificil') {
+    criaMosquitoTempo = 1000
+} else if (nivel === 'chucknoris') {
+    criaMosquitoTempo = 750
+}
+
+function ajustTamanhoTelaJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(largura, altura)
+}
+
+ajustTamanhoTelaJogo()
+
+
+function posicaoRandomica() {
+
+    //remover mosquito anterior (caso exista)
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+            //logica game over
+        if (vidas > 3) {
+
+            window.location.href = '/fim_de_jogo.html'
+
+        } else {
+            //logica remocao de vidas
+            document.getElementById('v' + vidas).src = '/imagens/coracao_vazio.png'
+            vidas++
+        }
+
+    }
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoy = posicaoY < 0 ? 0 : posicaoY
+
+
+
+    //criacao do elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = "/imagens/mosca.png"
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+        pontos += 1
+        document.getElementById('pontos').innerHTML = pontos
+
+    }
+
+    document.body.appendChild(mosquito)
+
+}
+
+/*Função de tempo do cronometro */
+var cronometro = setInterval(function() {
+    tempo -= 1
+    if (tempo <= 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = '/vitoria.html'
+
+    }
+    document.getElementById('cronometro').innerHTML = tempo
+
+
+
+}, 1000)
+
+
+
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+
+    }
+}
